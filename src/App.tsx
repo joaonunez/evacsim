c30e5271f18d7279140f208c193c013866480992
mdalyy <abibell2008@gmail.com>
2025-05-26 21:40:35 -0500

    Whast your mind?

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <div className="max-w-md mx-auto mt-10 p-4 border rounded">
         <textarea
-          placeholder="Escribe tu comentario..."
+          placeholder="Envia tu propuesta"
           className="w-full h-24 p-2 border rounded resize-none focus:outline-none focus:ring-2 focus:ring-red-500"
         ></textarea>
         <div className="mt-2 flex justify-end">
