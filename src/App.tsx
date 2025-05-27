aa7e4bd589a1d9f0d212f56b9d6af4855fb34595
mdalyy <abibell2008@gmail.com>
2025-05-27 18:25:37 -0500

    jjk

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ function App() {
     <>
       <div className="max-w-md mx-auto mt-10 p-4 border rounded">
         <textarea
-<<<<<<< HEAD
           placeholder="Whats your mind?"
-=======
-          placeholder="Evita los errores ortograficos..."
->>>>>>> 729643d5fab7e568ee99dc1403e77c6692cf7202
           className="w-full h-24 p-2 border rounded resize-none focus:outline-none focus:ring-2 focus:ring-red-500"
         ></textarea>
         <div className="mt-2 flex justify-end">
